37c60f8f7255d696fc01431f902a6e455d1d5cd1
Jake Ranallo <jake.ranallo@gmail.com>
2020-04-27 19:57:26 +0200

    add receipe tabs

--- src/screens/RecipeScreen.tsx
+++ src/screens/RecipeScreen.tsx
@@ -1,13 +1,53 @@
 import React from 'react';
-import { Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { Text, Button, View, ScrollView, Image, TouchableOpacity } from 'react-native';
 import { useNavigationParam } from 'react-navigation-hooks'
+
+const IngredientsTab = ({ recipe }) => {
+  return (
+    <View>
+      {recipe.ingredients && recipe.ingredients.map((ingredient, i) =>
+        <View>
+          <Text key={i}>{ingredient.subhead}</Text>
+          <View>{ingredient.list.map((item, i) =>
+            <Text key={i}>{item}</Text>)}
+          </View>
+        </View>)}
+    </View>
+  );
+}
+
+const ResultsTab = ({ recipe }) => {
+  return (
+    <View>
+      {recipe.results && recipe.results.map((result, i) =>
+        <Image
+          key={i}
+          source={{ uri: result }}
+          style={{ width: 100, height: 100 }} />)}
+    </View>
+  );
+}
 
 export const RecipeScreen = () => {
   const recipe = useNavigationParam('recipe');
+  const [isActive, setActive] = React.useState(1)
   return (
-    <ScrollView>
-      <Text>{recipe.title}</Text>
-      <Text>{recipe.description}</Text>
-    </ScrollView>
+    <>
+      <View>
+        <Text>{recipe.title}</Text>
+        <Text>{recipe.description}</Text>
+        <TouchableOpacity
+          onPress={() => setActive(1)}>
+          <Text style={{ color: isActive == 1 ? 'red' : 'white' }}>Ingredients</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setActive(2)}>
+          <Text style={{ color: isActive == 2 ? 'red' : 'white' }}>Results</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView>
+        {isActive === 1 ? <IngredientsTab recipe={recipe} /> : <ResultsTab recipe={recipe} />}
+      </ScrollView>
+    </>
   );
 }
