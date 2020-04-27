2f466bfe7650fe8626cafc6121418ee8d511e5d5
Jake Ranallo <jake.ranallo@gmail.com>
2020-04-27 18:06:44 +0200

    font setup

--- src/screens/FeedScreen.tsx
+++ src/screens/FeedScreen.tsx
@@ -1,39 +1,46 @@
 import React from 'react';
-import { Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { Text, View, ScrollView, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { recipes } from '../utils/mockData'
 import { NavigationInjectedProps } from 'react-navigation'
+import { useFonts } from '@use-expo/font';
 
 export const FeedScreen = ({ navigation }: NavigationInjectedProps) => {
+  const [isLoaded] = useFonts({ 'Neuton-Bold': require('../../assets/fonts/Neuton-Bold.ttf'), });
+
   return (
-    <ScrollView>
-      <Text>Latest Recipes</Text>
-      {recipes ? recipes.map((recipe) =>
-        <TouchableOpacity
-          key={recipe.id}
-          onPress={() => {
-            navigation.navigate('Recipe', {
-              recipe: recipe,
-            });
-          }}>
-          <View>
-            <Image
-              source={{ uri: recipe.imgOne }}
-              style={{ width: '100%', height: 400 }} />
-            <Image
-              source={{ uri: recipe.imgTwo }}
-              style={{ width: 100, height: 100 }} />
-            <Image
-              source={{ uri: recipe.imgThree }}
-              style={{ width: 100, height: 100 }} />
-          </View>
-          <Text>{recipe.title}</Text>
-          <Image
-            source={{ uri: recipe.user.avatar }}
-            style={{ width: 100, height: 100 }} />
-          <Text>{recipe.user.name}</Text>
-        </TouchableOpacity>
-      ) :
-        <Text>Loading...</Text>}
-    </ScrollView>
+    <View>
+      {isLoaded &&
+        <ScrollView>
+          <Text style={{ fontFamily: 'Neuton-Bold', fontSize: 40, color: 'white' }}>Latest Recipes</Text>
+          {recipes ? recipes.map((recipe) =>
+            <TouchableOpacity
+              key={recipe.id}
+              onPress={() => {
+                navigation.navigate('Recipe', {
+                  recipe: recipe,
+                });
+              }}>
+              <View>
+                <Image
+                  source={{ uri: recipe.imgOne }}
+                  style={{ width: '100%', height: 400 }} />
+                <Image
+                  source={{ uri: recipe.imgTwo }}
+                  style={{ width: 100, height: 100 }} />
+                <Image
+                  source={{ uri: recipe.imgThree }}
+                  style={{ width: 100, height: 100 }} />
+              </View>
+              <Text>{recipe.title}</Text>
+              <Image
+                source={{ uri: recipe.user.avatar }}
+                style={{ width: 100, height: 100 }} />
+              <Text>{recipe.user.name}</Text>
+            </TouchableOpacity>
+          ) :
+            <Text>Loading...</Text>}
+        </ScrollView>
+      }
+    </View>
   );
 }
